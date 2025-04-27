ff40bab70f6cb9d35bbc171c7d1673e74a57c94e
albert <albert@rocareer.com>
2025-04-28 02:31:07 +0800

    feat(config): 添加 webman-dev命令行工具支持
    - 在 src/config/plugin/rocareer/webman-dev/ 目录下新增 command.php 文件
    - 添加 Rocareer\WebmanDev\command\RocareerPlugin 类以支持命令行工具功能

---
+++ src/config/plugin/rocareer/webman-dev/command.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * File      command.php
+ * Author    albert@rocareer.com
+ * Time      2025-04-28 02:27:52
+ * Describe  command.php
+ */
+
+return[
+   \Rocareer\WebmanDev\command\RocareerPlugin::class
+];
